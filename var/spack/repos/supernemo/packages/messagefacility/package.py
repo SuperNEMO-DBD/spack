874db84d092ab0ea98b6e0125e60444d0dfbce27
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2020-03-19 16:29:50 +0000

    messagefacility: new package

---
+++ var/spack/repos/supernemo/packages/messagefacility/package.py
@@ -0,0 +1,101 @@
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+import os
+
+
+def sanitize_environments(*args):
+    for env in args:
+        for var in ('PATH', 'CET_PLUGIN_PATH',
+                    'LD_LIBRARY_PATH', 'DYLD_LIBRARY_PATH', 'LIBRARY_PATH',
+                    'CMAKE_INSTALL_RPATH', 'CMAKE_PREFIX_PATH', 'ROOT_INCLUDE_PATH'):
+            env.prune_duplicate_paths(var)
+            env.deprioritize_system_paths(var)
+
+
+class Messagefacility(CMakePackage):
+    """A configurable message logging facility for the art suite."""
+
+    homepage = 'http://art.fnal.gov/'
+    git_base = 'http://cdcvs.fnal.gov/projects/messagefacility'
+
+    version('MVP1a', branch='feature/Spack-MVP1a',
+            git=git_base, preferred=True)
+    version('MVP', branch='feature/for_spack', git=git_base)
+    version('develop', branch='develop', git=git_base)
+    version('2.03.00', tag='v2_03_00', git=git_base)
+    version('2.03.01', tag='v2_03_01', git=git_base)
+    version('2.04.03', tag='v2_04_03', git=git_base)
+    version('2.05.00', tag='v2_05_00', git=git_base)
+
+    _cxxstds = ('14', '17')
+    variant('cxxstd',
+            default='17',
+            values=_cxxstds,
+            multi=False,
+            description='Use the specified C++ standard when building.')
+
+    # Build-only dependencies.
+    depends_on('cmake@3.11:', type='build')
+    depends_on('cetmodules@1.01.01:', type='build')
+    depends_on('catch2@2.3.0:', type='build')
+
+    # Build / link dependencies.
+    for s in _cxxstds:
+      depends_on('cetlib-except cxxstd=' + s, when='cxxstd=' + s)
+      depends_on('cetlib cxxstd=' + s, when='cxxstd=' + s)
+      depends_on('boost cxxstd=' + s, when='cxxstd=' + s)
+      depends_on('fhicl-cpp cxxstd=' + s, when='cxxstd=' + s)
+      depends_on('hep-concurrency cxxstd=' + s, when='cxxstd=' + s)
+      depends_on('tbb cxxstd=' + s, when='cxxstd=' + s)
+    
+    depends_on('sqlite')
+    depends_on('perl')
+
+    if 'SPACKDEV_GENERATOR' in os.environ:
+        generator = os.environ['SPACKDEV_GENERATOR']
+        if generator.endswith('Ninja'):
+            depends_on('ninja', type='build')
+
+    def url_for_version(self, version):
+        url = 'http://cdcvs.fnal.gov/cgi-bin/git_archive.cgi/cvs/projects/{0}.v{1}.tbz2'
+        return url.format(self.name, version.underscored)
+
+    def cmake_args(self):
+        # Set CMake args.
+        args = ['-DCMAKE_CXX_STANDARD={0}'.
+                format(self.spec.variants['cxxstd'].value)]
+        return args
+
+    def setup_environment(self, spack_env, run_env):
+        # Binaries.
+        spack_env.prepend_path('PATH',
+                               os.path.join(self.build_directory, 'bin'))
+        # Ensure we can find plugin libraries.
+        spack_env.prepend_path('CET_PLUGIN_PATH',
+                               os.path.join(self.build_directory, 'lib'))
+        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        # Perl modules.
+        spack_env.prepend_path('PERL5LIB',
+                               os.path.join(self.build_directory, 'perllib'))
+        run_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        # Cleaup.
+        sanitize_environments(spack_env, run_env)
+
+    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
+        # Binaries.
+        spack_env.prepend_path('PATH', self.prefix.bin)
+        run_env.prepend_path('PATH', self.prefix.bin)
+        spack_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        run_env.prepend_path('ROOT_INCLUDE_PATH', self.prefix.include)
+        # Ensure we can find plugin libraries.
+        spack_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        run_env.prepend_path('CET_PLUGIN_PATH', self.prefix.lib)
+        # Perl modules.
+        spack_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        run_env.prepend_path('PERL5LIB', os.path.join(self.prefix, 'perllib'))
+        # Cleanup.
+        sanitize_environments(spack_env, run_env)
